bee63145c75aaddee1b03d8aaece8ec0eb392e9b
ChampionAsh5357 <unconfigured@null.spigotmc.org>
2017-07-31 23:59:20 -0400

    Fixed Code

--- main/java/com/championash5357/tutorial/enchantment/EnchantmentFly.java
+++ main/java/com/championash5357/tutorial/enchantment/EnchantmentFly.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.championash5357.tutorial.enchantment;
 
 import com.championash5357.tutorial.client.Reference;
@@ -31,37 +30,3 @@ public class EnchantmentFly extends Enchantment{
 		return 1;
 	}
 }
-=======
-package com.championash5357.tutorial.enchantment;
-
-import com.championash5357.tutorial.client.Reference;
-
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnumEnchantmentType;
-import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.util.ResourceLocation;
-
-public class EnchantmentFly extends Enchantment{
-
-	public EnchantmentFly(Rarity rarityIn, EntityEquipmentSlot[] slots) {
-		super(rarityIn, EnumEnchantmentType.ARMOR_FEET, slots);
-		this.setName("fly");
-		this.setRegistryName(new ResourceLocation(Reference.MOD_ID + ":" + "fly"));
-	}
-	
-	@Override
-	public int getMinEnchantability(int enchantmentLevel) {
-		return 30 * enchantmentLevel;
-	}
-	
-	@Override
-	public int getMaxEnchantability(int enchantmentLevel) {
-		return this.getMinEnchantability(enchantmentLevel);
-	}
-	
-	@Override
-	public int getMaxLevel() {
-		return 1;
-	}
-}
->>>>>>> bdf34e8316fefd9ef2339fca27319eec751ce5df
--- main/java/com/championash5357/tutorial/init/TutorialItems.java
+++ main/java/com/championash5357/tutorial/init/TutorialItems.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.championash5357.tutorial.init;
 
 import com.championash5357.tutorial.item.ItemFlyArmor;
@@ -61,67 +60,3 @@ public class TutorialItems {
 		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
 	}
 }
-=======
-package com.championash5357.tutorial.init;
-
-import com.championash5357.tutorial.item.ItemFlyArmor;
-import com.championash5357.tutorial.item.ItemStrawberry;
-import com.championash5357.tutorial.item.ItemStrawberrySeeds;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.SoundEvents;
-import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemArmor.ArmorMaterial;
-import net.minecraftforge.common.util.EnumHelper;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
-
-public class TutorialItems {
-	
-	public static ArmorMaterial FlyArmor = EnumHelper.addArmorMaterial("FlyArmor", "tutorial:fly", 40, new int[]{1,5,3,1}, 20, SoundEvents.BLOCK_ANVIL_LAND, 0.0f);
-	public static Item fly_helmet;
-	public static Item fly_chestplate;
-	public static Item fly_leggings;
-	public static Item fly_boots;
-	
-	public static Item strawberry_seeds;
-	public static Item strawberry;
-	
-	public static void init() {
-		strawberry_seeds = new ItemStrawberrySeeds(TutorialBlocks.strawberry_crop, Blocks.FARMLAND);
-		strawberry = new ItemStrawberry(2, 1.0f, false);
-		fly_helmet = new ItemFlyArmor(FlyArmor, 1, EntityEquipmentSlot.HEAD).setUnlocalizedName("fly_helmet").setRegistryName("itemflyhelmet");
-		fly_chestplate = new ItemFlyArmor(FlyArmor, 1, EntityEquipmentSlot.CHEST).setUnlocalizedName("fly_chestplate").setRegistryName("itemflychestplate");
-		fly_leggings = new ItemFlyArmor(FlyArmor, 2, EntityEquipmentSlot.LEGS).setUnlocalizedName("fly_leggings").setRegistryName("itemflyleggings");
-		fly_boots = new ItemFlyArmor(FlyArmor, 1, EntityEquipmentSlot.FEET).setUnlocalizedName("fly_boots").setRegistryName("itemflyboots");
-	}
-	
-	public static void register() {
-		registerItem(strawberry_seeds);
-		registerItem(strawberry);
-		registerItem(fly_helmet);
-		registerItem(fly_chestplate);
-		registerItem(fly_leggings);
-		registerItem(fly_boots);
-	}
-	
-	private static void registerItem(Item item) {
-		ForgeRegistries.ITEMS.register(item);
-	}
-	
-	public static void registerRenders() {
-		registerRender(strawberry_seeds);
-		registerRender(strawberry);
-		registerRender(fly_helmet);
-		registerRender(fly_chestplate);
-		registerRender(fly_leggings);
-		registerRender(fly_boots);
-	}
-	
-	public static void registerRender(Item item) {
-		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
-	}
-}
->>>>>>> bdf34e8316fefd9ef2339fca27319eec751ce5df
